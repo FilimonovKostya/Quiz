e5329978ffd219ef573916c6a90bca16b25fc47f
Filimonov Kostya <konstantinfilimonou@gmail.com>
2020-12-21 18:49:53 +0300

    refactoring

--- src/Answer.tsx
+++ src/Answer.tsx
@@ -1,41 +1,19 @@
 import React from "react";
-import {StateType} from "./App";
+
 
 type AnswerPropsType = {
-    answer: StateType
-    questionsID: string
-    changeAnswerAndQuestion: (questionID: string, answerID: string) => void
-    setRight: (value: string) => void
-    setWrong: (value: string) => void
-    right: string
-    wrong: string
-    setLoading:(boolean:boolean) => void
+    answers: string[]
+
 }
 
 export const Answer = (props: AnswerPropsType) => {
 
+    const answers = props.answers.map((el, index) => <li key={index} > {el} </li>)
+
 
     return <div className={'answerContainer'}>
         <ul className={'answers'}>
-            {
-                props.answer[props.questionsID].map((el, index) => {
-                    const onClickHandler = () => {
-
-                        if (el.isRight) {
-                            props.changeAnswerAndQuestion(props.questionsID, el.id)
-                            props.setRight('right')
-                            props.setLoading(true)
-
-                        } else {
-                            props.changeAnswerAndQuestion(props.questionsID, el.id)
-                            props.setWrong('wrong')
-                            props.setLoading(true)
-
-                        }
-                    }
-                    return <li key={index} className={el.isRight ? props.right : ''} onClick={onClickHandler}> {el.answer}</li>
-                })
-            }
+            {answers}
         </ul>
     </div>
 }
--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,107 @@
-import React from 'react';
-import {Answer} from './Answer';
+import React, {useState} from 'react';
 import './App.css';
 import {Question} from './Question';
+import {Answer} from './Answer';
+import {v1} from "uuid";
+
+type AnswersType = {
+    id: string
+    answer: string
+    isRight: boolean
+}
+
+export type InitialStateType = {
+    id: string
+    title: string
+    answers: AnswersType[]
+}
 
 
 function App() {
+
+    const firstQuestionID = v1()
+    const secondQuestionID = v1()
+    const thirdQuestionID = v1()
+
+    const [currentValue, setCurrentValue] = useState<number>(0)
+    const [countRightAnswers, setCountRightAnswers] = useState<number>(0)
+
+
+    const [state, setState] = useState<InitialStateType[]>([
+        {
+            id: firstQuestionID, title: 'Что такое React',
+            answers: [
+                {
+                    id: firstQuestionID,
+                    answer: 'Это декларативная, эффективная и гибкая JavaScript библиотека для создания пользовательских интерфейсов.',
+                    isRight: true
+                },
+                {
+                    id: v1(),
+                    answer: 'Это полноценный фрейморк',
+                    isRight: false
+                },
+                {
+                    id: v1(), answer: 'Это вообще язык программирования ',
+                    isRight: false
+                }
+            ]
+        },
+        {
+            id: secondQuestionID, title: 'Почему мы используем React',
+            answers: [
+                {
+                    id: v1(),
+                    answer: 'Так как нет других варинатов',
+                    isRight: false
+                },
+                {
+                    id: secondQuestionID,
+                    answer: 'Из-за скорости работы ,благодаря Virtual Dom',
+                    isRight: true
+                },
+                {
+                    id: v1(), answer: 'Больше платят и он на хайпе', isRight: false
+                }
+            ]
+        },
+        {
+            id: thirdQuestionID,
+            title: 'Что такое Virtual Dom',
+            answers: [
+                {
+                    id: v1(),
+                    answer: 'Аналог на Shadow DOM',
+                    isRight: false
+                },
+                {
+                    id: v1(),
+                    answer: 'это браузерная технология, предназначенная в основном для определения области видимости переменных и CSS в веб-компонентах.',
+                    isRight: false
+                },
+                {
+                    id: thirdQuestionID,
+                    answer: "это техника и набор библиотек  алгоритмов, которые позволяют нам улучшить производительность на клиентской стороне",
+                    isRight: true
+                }
+            ]
+        },
+
+    ])
+
 
 
     return <div className={'wrapper'}>
         <div className={'container'}>
 
             <React.Fragment>
-                <Question question={titleForQuestion[currentQuestion]}/>
+                {
+                    state.length <= currentValue
+                        ? <h1>{countRightAnswers}</h1>
+                        : <><Question question={state[currentValue].title}/>
+                            <Answer answers={state[currentValue].answers.map(el => el.answer)}/></>
+                }
 
-                <Answer answer={state} questionsID={answersID[currentQuestion]}
-                        changeAnswerAndQuestion={changeAnswerAndQuestion}
-                        setRight={setRight} setWrong={setWrong}
-                        right={right} wrong={wrong}
-                        setLoading={setLoading}
-                />
             </React.Fragment>
 
         </div>
